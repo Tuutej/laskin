0622c143b19455078771a23b9c840735fa0ebfc4
Tuutej <90647045+Tuutej@users.noreply.github.com>
2023-08-27 12:20:55 +0300

    laskin lisätty

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, Button, Alert, TextInput, StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
-export default function App() {
+const SimpleCalc = () => {
+  const [num1, setNum1] = useState("");
+  const [num2, setNum2] = useState("");
+  const [result, setResult] = useState("");
+
+  const addition = () => {
+    const sum = parseFloat(num1) + parseFloat(num2);
+    setResult(`Summa: ${sum}`);
+  };
+
+  const subtraction = () => {
+    const difference = parseFloat(num1) - parseFloat(num2);
+    setResult(`Ero: ${difference}`);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ marginTop: 20 }}>{result}</Text>
+      <TextInput
+        keyboardType="numeric"
+        placeholder="Syötä numero"
+        value={num1}
+        onChangeText={setNum1}
+      />
+      <View style={{ marginTop: 20 }} />
+      <TextInput
+        keyboardType="numeric"
+        placeholder="Syötä numero"
+        value={num2}
+        onChangeText={setNum2}
+      />
+      <View style={styles.buttonContainer}>
+        <Button title="+" onPress={addition} style={styles.button} />
+        <View style={{ width: 40 }} />
+        <Button title="-" onPress={subtraction} style={styles.button} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+export default SimpleCalc;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 20,
   },
 });
