6af980cb17f500db0a557d18bb792fd47a7bf6b2
Tuutej <90647045+Tuutej@users.noreply.github.com>
2023-09-10 15:00:19 +0300

    added navigation

--- App.js
+++ App.js
@@ -8,57 +8,94 @@ import {
   FlatList,
 } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-const SimpleCalc = () => {
+const Stack = createNativeStackNavigator();
+
+export default function App() {
   const [num1, setNum1] = useState("");
   const [num2, setNum2] = useState("");
   const [result, setResult] = useState("");
   const [data, setData] = useState([]);
 
-  const addition = () => {
-    const sum = parseFloat(num1) + parseFloat(num2);
-    setResult(`Summa: ${sum}`);
-    setData([...data, `Summa: ${num1} + ${num2} = ${sum}`]);
-  };
+  function Calculator({ navigation, route }) {
+    const { params } = route;
 
-  const subtraction = () => {
-    const difference = parseFloat(num1) - parseFloat(num2);
-    setResult(`Ero: ${difference}`);
-    setData([...data, `Ero: ${num1} - ${num2} = ${difference}`]);
-  };
+    const addition = () => {
+      const sum = parseFloat(num1) + parseFloat(num2);
+      setResult(`Summa: ${sum}`);
+      setData([...data, `Summa: ${num1} + ${num2} = ${sum}`]);
+    };
+
+    const subtraction = () => {
+      const difference = parseFloat(num1) - parseFloat(num2);
+      setResult(`Ero: ${difference}`);
+      setData([...data, `Ero: ${num1} - ${num2} = ${difference}`]);
+    };
+
+    return (
+      <View style={styles.container}>
+        <Text style={{ marginTop: 20 }}>{result}</Text>
+        <TextInput
+          keyboardType="numeric"
+          placeholder="Syötä numero"
+          value={num1}
+          onChangeText={setNum1}
+        />
+        <View style={{ marginTop: 20 }} />
+        <TextInput
+          keyboardType="numeric"
+          placeholder="Syötä numero"
+          value={num2}
+          onChangeText={setNum2}
+        />
+        <View style={styles.buttonContainer}>
+          <Button title="+" onPress={addition} />
+          <View style={{ width: 12 }} />
+          <Button title="-" onPress={subtraction} />
+          <View style={{ width: 12 }} />
+          <Button
+            title="History"
+            onPress={() => navigation.navigate("History")}
+          />
+        </View>
+      </View>
+    );
+  }
+
+  function History({ route }) {
+    const { params } = route;
+    return (
+      <View style={styles.container}>
+        <Text>History</Text>
+        <FlatList
+          data={params.data}
+          renderItem={({ item }) => <Text>{item}</Text>}
+          keyExtractor={(item, index) => index.toString()}
+        />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 
   return (
-    <View style={styles.container}>
-      <Text style={{ marginTop: 20 }}>{result}</Text>
-      <TextInput
-        keyboardType="numeric"
-        placeholder="Syötä numero"
-        value={num1}
-        onChangeText={setNum1}
-      />
-      <View style={{ marginTop: 20 }} />
-      <TextInput
-        keyboardType="numeric"
-        placeholder="Syötä numero"
-        value={num2}
-        onChangeText={setNum2}
-      />
-      <View style={styles.buttonContainer}>
-        <Button title="+" onPress={addition} />
-        <View style={{ width: 40 }} />
-        <Button title="-" onPress={subtraction} />
-      </View>
-      <FlatList
-        data={data}
-        renderItem={({ item }) => <Text>{item}</Text>}
-        keyExtractor={(item, index) => index.toString()}
-      />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Calculator"
+          component={Calculator}
+          initialParams={{ data, setData }}
+        />
+        <Stack.Screen
+          name="History"
+          component={History}
+          initialParams={{ data }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
-
-export default SimpleCalc;
+}
 
 const styles = StyleSheet.create({
   container: {
